3558953603a08fe62568779bb545bb3ba1a8ce30
Thinus <thinusvdv2001@gmail.com>
2023-01-11 16:52:16 +0200

    Leaderboard project setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ]
+    }
+}
