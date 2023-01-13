e405be987a154d628c496e57bc112ff33c6f7a00
Thinus <thinusvdv2001@gmail.com>
2023-01-13 17:01:04 +0200

    Add API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import { fetchData, sendData } from './modules/list.js';
+
+const refreshBtn = document.getElementById('RefreshButton');
+const form = document.getElementById('submit');
+
+refreshBtn.addEventListener('click', fetchData);
+form.addEventListener('submit', sendData);
